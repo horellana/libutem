34d44350bfe7c8a89e576c462cd1d489d9bea9e8
Hector Orellana <pulga.p.29@gmail.com>
2015-12-22 02:15:25 -0300

    Creada clase dirdoc.Cliente

--- programa/src/dirdoc/__init__.py
+++ programa/src/dirdoc/__init__.py
@@ -2,22 +2,30 @@ import requests
 
 
 class ErrorLogin(Exception):
-    def __init__(self, response):
+    def __init__(self, response, rut, contrasena):
         error = 'Ocurrio un error al ingresar, verifica usuario y contraseña.'
-        info = '<url: {}>'.format(response.url)
+        info = 'rut: {}, contraseña: {}, url: {}'.format(rut, contrasena, response.url)
         Exception.__init__(self, '{}\nInformacion Adicional: {}'
                            .format(error, info))
 
-def login(rut, contrasena):
-    url = 'http://mi.utem.cl/login'
-    user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
-    r = requests.post(url,
-                      data={'rut_alumno': rut, 'contrasena': contrasena},
-                      headers={'user-agent': user_agent})
+class Cliente:
+    def __init__(self):
+        self.cookies = {}
+        self.logueado = False
+        self.ultima_respuesta = {}
 
-    if not r.status_code == requests.codes.ok:
-        r.raise_for_status()
-    elif not r.url == 'http://mi.utem.cl/inicio':
-        raise ErrorLogin(r)
-    else:
-        return r
+    def login(self, rut, contrasena):
+        url = 'http://mi.utem.cl/login'
+        user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
+        r = requests.post(url,
+                          data={'rut_alumno': rut, 'contrasena': contrasena},
+                          headers={'user-agent': user_agent})
+
+        if not r.status_code == requests.codes.ok:
+            r.raise_for_status()
+        elif not r.url == 'http://mi.utem.cl/inicio':
+            raise ErrorLogin(r, rut, contrasena)
+        else:
+            self.logueado = True
+            self.cookies = r.cookies
+            self.ultima_respuesta = r
