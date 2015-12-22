69feb280b6082cd3ccde459da7b5e8bfdf23a39c
Hector Orellana <pulga.p.29@gmail.com>
2015-12-22 01:09:08 -0300

    Lanzar error si es que no se pudo ingresar.

--- programa/src/dirdoc/__init__.py
+++ programa/src/dirdoc/__init__.py
@@ -3,3 +3,7 @@ import requests
 def login(usuario, contrasena):
     url = 'http://mi.utem.cl/login'
     return requests.post(url, {'usuario': usuario, 'contrasena': contrasena})
+    if not r.status_code == requests.codes.ok:
+        r.raise_for_status()
+    else:
+        return r
