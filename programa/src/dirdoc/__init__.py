bda4e0402e3809b5a339fa7c2e968daab221c667
Hector Orellana <pulga.p.29@gmail.com>
2015-12-22 01:31:35 -0300

    Corregida funcion login

--- programa/src/dirdoc/__init__.py
+++ programa/src/dirdoc/__init__.py
@@ -1,11 +1,11 @@
 import requests
 
-def login(usuario, contrasena):
+def login(rut, contrasena):
     url = 'http://mi.utem.cl/login'
-    user-agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
     r = requests.post(url,
-                      {'usuario': usuario, 'contrasena': contrasena},
-                      headers={'user-agent': user-agent})
+                      data={'rut_alumno': rut, 'contrasena': contrasena},
+                      headers={'user-agent': user_agent})
 
     if not r.status_code == requests.codes.ok:
         r.raise_for_status()
