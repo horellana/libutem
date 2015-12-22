d86e0811eca6030a41f8424ad90a10ad40d81fd3
Hector Orellana <pulga.p.29@gmail.com>
2015-12-22 01:58:01 -0300

    Nueva clase que representa error en el login

--- programa/src/dirdoc/__init__.py
+++ programa/src/dirdoc/__init__.py
@@ -1,5 +1,13 @@
 import requests
 
+
+class ErrorLogin(Exception):
+    def __init__(self, response):
+        error = 'Ocurrio un error al ingresar, verifica usuario y contraseña.'
+        info = '<url: {}>'.format(response.url)
+        Exception.__init__(self, '{}\nInformacion Adicional: {}'
+                           .format(error, info))
+
 def login(rut, contrasena):
     url = 'http://mi.utem.cl/login'
     user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
@@ -9,5 +17,7 @@ def login(rut, contrasena):
 
     if not r.status_code == requests.codes.ok:
         r.raise_for_status()
+    elif not r.url == 'http://mi.utem.cl/inicio':
+        raise ErrorLogin(r)
     else:
         return r
