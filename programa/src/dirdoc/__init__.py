7578e4fd740a113a260e56ad39cb52307cf0af0a
Hector Orellana <pulga.p.29@gmail.com>
2015-12-22 01:10:06 -0300

    Utilizar un user-agent apropiado

--- programa/src/dirdoc/__init__.py
+++ programa/src/dirdoc/__init__.py
@@ -2,7 +2,11 @@ import requests
 
 def login(usuario, contrasena):
     url = 'http://mi.utem.cl/login'
-    return requests.post(url, {'usuario': usuario, 'contrasena': contrasena})
+    user-agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
+    r = requests.post(url,
+                      {'usuario': usuario, 'contrasena': contrasena},
+                      headers={'user-agent': user-agent})
+
     if not r.status_code == requests.codes.ok:
         r.raise_for_status()
     else:
