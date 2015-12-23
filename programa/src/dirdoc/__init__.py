ca4651f82611ee05bc746c4edb0bea3d36073989
Hector Orellana <pulga.p.29@gmail.com>
2015-12-23 00:52:00 -0300

    Ahora se utiliza una requests.Session().

--- programa/src/dirdoc/__init__.py
+++ programa/src/dirdoc/__init__.py
@@ -23,13 +23,15 @@ class Cliente:
         self.cookies = {}
         self.logueado = False
         self.ultima_respuesta = {}
+        self.session = requests.Session()
+        self.session.headers.update({
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
+        })
 
     def login(self, rut, contrasena):
-        url = 'http://mi.utem.cl/login'
-        user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
-        r = requests.post(url,
-                          data={'rut_alumno': rut, 'contrasena': contrasena},
-                          headers={'user-agent': user_agent})
+        r = self.session.post('http://mi.utem.cl/login',
+                              data={'rut_alumno': rut, 'contrasena': contrasena})
+
 
         if not r.status_code == requests.codes.ok:
             r.raise_for_status()
@@ -37,7 +39,6 @@ class Cliente:
             raise ErrorLogin(r, rut, contrasena)
         else:
             self.logueado = True
-            self.cookies = r.cookies
             self.ultima_respuesta = r
 
     @requiere_login
