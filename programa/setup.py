9c010a62efcece9bb080b2929c86d23f7e628132
Hector Orellana <pulga.p.29@gmail.com>
2015-12-27 05:02:17 -0300

    Aumento el numero de la version

--- programa/setup.py
+++ programa/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='comprobar-estudiante',
-    version='0.1.3',
+    version='0.1.7',
     author='Hector Orellana',
     author_email='hofm92@gmail.com',
     url='https://github.com/juiko/comprobar-estudiante',
