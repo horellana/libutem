e73aeb4ab2d9d1addaaa781f4c81d9390b03c482
Hector Orellana <pulga.p.29@gmail.com>
2015-12-27 00:10:00 -0300

    Agregados setup.py y requeriments.txt

---
+++ programa/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='comprobar-estudiante',
+    version='0.0.1',
+    author='Hector Orellana',
+    author_email='hofm92@gmail.com',
+    url='https://github.com/juiko/comprobar-estudiante',
+    license='GPL3',
+    packages=['miutem'],
+    package_dir={'miutem': 'src/miutem'},
+    scripts=['bin/estudiante.py'],
+    install_requires=['pyquery', 'requests']
+)
