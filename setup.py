b9e38c88e3ccff77795ad1ed6e9596c14951df15
Hector Orellana <pulga.p.29@gmail.com>
2015-12-30 23:43:20 -0300

    Incrementado el numero de la version

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup
 
 setup(
     name='libutem',
-    version='0.1.10',
+    version='0.1.19',
     author='Hector Orellana',
     author_email='hofm92@gmail.com',
     url='https://github.com/juiko/libutem',
     license='GPL3',
-    packages=['utem', 'utem.miutem', 'utem.dirdoc'],
+    packages=['utem',
+              'utem.miutem',
+              'utem.dirdoc'],
     scripts=['bin/estudiante.py'],
-    install_requires=['pyquery', 'requests']
+    install_requires=['pyquery',
+                      'requests']
 )
