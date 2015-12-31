234c18be99ebbba2b4e86afca7a8c84a759ff1a5
Hector Orellana <pulga.p.29@gmail.com>
2015-12-30 23:36:19 -0300

    Actualizados los tests

--- test/test_miutem.py
+++ test/test_miutem.py
@@ -1,21 +1,21 @@
 import pytest
-import miutem
-import miutem.html
+import utem
+import utem.miutem
 
 
 def test_cliente_notas():
-    cliente = miutem.Cliente()
+    cliente = utem.miutem.Cliente()
 
-    with pytest.raises(miutem.FaltaLogin):
+    with pytest.raises(utem.errores.FaltaLogin):
         cliente.notas()
 
 
-def test_extraer_informacion_ramo_uno():
+def test_extraer_inwformacion_ramo_uno():
     texto = 'Curso: INDC8060 - SISTEMAS ECONÓMICOS (411)'
     esperado = {'codigo': 'INDC8060',
                 'nombre': 'SISTEMAS ECONÓMICOS',
                 'seccion': '411'}
-    obtenido = miutem.html.extraer_informacion_ramo(texto)
+    obtenido = utem.miutem.html.extraer_informacion_ramo(texto)
 
     assert obtenido is not None
     assert obtenido['codigo'] == esperado['codigo']
@@ -28,7 +28,7 @@ def test_extraer_informacion_ramo_dos():
     esperado = {'codigo': 'FISC8040',
                 'nombre': 'ELECTROMAGNETISMO',
                 'seccion': '103'}
-    obtenido = miutem.html.extraer_informacion_ramo(texto)
+    obtenido = utem.miutem.html.extraer_informacion_ramo(texto)
 
     assert obtenido is not None
     assert obtenido['codigo'] == esperado['codigo']
