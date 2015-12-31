1c2dd35ff8124505332159497a58b987a1e4b6b8
Hector Orellana <pulga.p.29@gmail.com>
2015-12-30 23:30:10 -0300

    login para dirdoc

--- utem/dirdoc/__init__.py
+++ utem/dirdoc/__init__.py
@@ -2,3 +2,21 @@
 Modulo dirdoc
 """
 
+import utem
+import utem.dirdoc.html as html
+from utem.utilidades import requiere_login
+
+
+class Cliente(utem.Cliente):
+
+    def login(self, rut, contraseña):
+        try:
+            self.peticion(url='http://postulacion.utem.cl/valida.php'
+                          data={'rut': rut, 'password': contraseña, 'tipo': '0'})
+            self.logueado = True
+        except ErrorPeticion as err:
+            raise ErrorLogin(err)
+
+    @requiere_login
+    def notas():
+        pass
