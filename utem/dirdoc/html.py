c8888cb03b7cedc51e8e3ac5d15f7ce507731127
Hector Orellana <pulga.p.29@gmail.com>
2016-01-07 00:07:42 -0300

    Funcion extraer_tabla_notas lista

--- utem/dirdoc/html.py
+++ utem/dirdoc/html.py
@@ -2,15 +2,31 @@
 Modulo utem.dirdoc.html
 """
 
+import re
 from pyquery import PyQuery as pq
-
+from utem.utilidades import arreglar_flotante
 
 def extraer_tabla_notas(html):
+    """
+    Recibe el html de la seccion de dirdoc que muestra la tabla
+    con los ramos del semestre actual.
+    Retorna un diccionario con toda la informacion.
+    """
     d = pq(html)
+    filas = d('table tr[align=center]')
 
-    filas = d('.pequena tbody tr')
+    for fila in filas:
+        columnas = fila.findall('td')
+        link = columnas[6].find('a').get('href')
+        datos = [dato.text for dato in columnas[:6]]
 
-    return None
+        yield {'codigo': datos[0],
+               'nombre': datos[1],
+               'profesor': datos[2],
+               'seccion': datos[3],
+               'estado': datos[4],
+               'promedio': float(arreglar_flotante(datos[5])),
+               'link_notas_parciales': link}
 
 def extraer_notas_parciales(html):
     d = pq(html)
